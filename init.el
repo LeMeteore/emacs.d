2192e39f76310ce75e7ab431f25499d032ac8e72
patrick nsukami <ndkpatt@gmail.com>
2015-04-24 14:38:02 +0000

    Reading emails with mu4e.

--- init.el
+++ init.el
@@ -4,6 +4,8 @@
 ;;(setq package-enable-at-startup nil)
 (package-initialize)
 (add-to-list 'load-path "~/.emacs.d/lisp")
+;; the exact path to mu4e
+(add-to-list 'load-path "/usr/local/share/emacs/site-lisp/mu4e")
 
 (load "init_load_packages")
 (load "init_elisp_functions")
