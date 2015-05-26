9e3bf2d7c602a5d95ab8f22bc536a3c66a4e2e29
patrick nsukami <ndkpatt@gmail.com>
2015-05-26 15:16:07 +0000

    Copy/cut using advice.

--- lisp/init_elisp_functions.el
+++ lisp/init_elisp_functions.el
@@ -474,3 +474,36 @@ This command does not push erased text to kill-ring."
     (move-beginning-of-line 1)
     (setq x2 (point))
     (delete-region x1 x2)))
+
+
+(defadvice kill-region (before slick-cut activate compile)
+  "When called interactively with no active region, kill a single line instead."
+  (interactive
+   (if mark-active
+       (list (region-beginning) (region-end))
+     (list (line-beginning-position) (line-beginning-position 2)))))
+
+(defadvice kill-ring-save (before slick-copy activate compile)
+  "When called interactively with no active region, copy a single line instead."
+  (interactive
+   (if mark-active
+       (list (region-beginning) (region-end))
+     (message "Copied line")
+     (list (line-beginning-position) (line-beginning-position 2)))))
+
+(defun slick-cut (beg end)
+  (interactive
+   (if mark-active
+       (list (region-beginning) (region-end))
+     (list (line-beginning-position) (line-beginning-position 2)))))
+
+(advice-add 'kill-region :before #'slick-cut)
+
+(defun slick-copy (beg end)
+  (interactive
+   (if mark-active
+       (list (region-beginning) (region-end))
+     (message "Copied line")
+     (list (line-beginning-position) (line-beginning-position 2)))))
+
+(advice-add 'kill-ring-save :before #'slick-copy)
