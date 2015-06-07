8d8ec6e13fd9f41ee8d464256502554333bea85d
patrick nsukami <ndkpatt@gmail.com>
2015-06-07 02:48:31 +0000

    Default font configuration.

--- lisp/init_font_lock.el
+++ lisp/init_font_lock.el
@@ -1,5 +1,31 @@
 ;; -*- coding: utf-8 -*-
 
+;;(set-frame-font "FiraCode-Regular-12" )
+;;(set-default-font "FiraCode-Regular-12")
+;; useful only when you do not use emacsclient
+
+;; the best way to set font when using emacsclient
+;; is to edit your ~/.Xdefaults file & put:
+;; Emacs.font: Inconsolata-12
+;; then, from your terminal:
+;; xrdb -merge ~/.Xdefaults
+
+;; set default font in initial window and for any new window
+
+;; (cond
+;;  ((string-equal system-type "windows-nt") ; Microsoft Windows
+;;   (when (member "DejaVu Sans Mono" (font-family-list))
+;;     (add-to-list 'initial-frame-alist '(font . "DejaVu Sans Mono-10"))
+;;     (add-to-list 'default-frame-alist '(font . "DejaVu Sans Mono-10"))))
+;;  ((string-equal system-type "darwin") ; Mac OS X
+;;   (when (member "DejaVu Sans Mono" (font-family-list))
+;;     (add-to-list 'initial-frame-alist '(font . "DejaVu Sans Mono-10"))
+;;     (add-to-list 'default-frame-alist '(font . "DejaVu Sans Mono-10"))))
+;;  ((string-equal system-type "gnu/linux") ; linux
+;;   (when (member "DejaVu Sans Mono" (font-family-list))
+;;     (add-to-list 'initial-frame-alist '(font . "DejaVu Sans Mono-10"))
+;;     (add-to-list 'default-frame-alist '(font . "DejaVu Sans Mono-10")))))
+
 
 (add-hook 'prog-mode-hook
           (lambda ()
