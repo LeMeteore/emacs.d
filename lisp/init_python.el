30c8835b4e5c4b31575ba26c93f803bad2727143
patrick nsukami <ndkpatt@gmail.com>
2015-07-23 02:40:38 +0000

    2 semicolons + 1 space for lisp comments.

--- lisp/init_python.el
+++ lisp/init_python.el
@@ -2,21 +2,21 @@
 
 (require 'python-mode)
 
-; use IPython
+;; use IPython
 (setq-default py-shell-name "ipython3")
 (setq-default py-which-bufname "IPython")
 
-; use the wx backend, for both mayavi and matplotlib
+;; use the wx backend, for both mayavi and matplotlib
 (setq py-python-command-args
-  '("--gui=wx" "--pylab=wx" "-colors" "Linux"))
+      '("--gui=wx" "--pylab=wx" "-colors" "Linux"))
 (setq py-force-py-shell-name-p t)
 
-; switch to the interpreter after executing code
-(setq py-shell-switch-buffers-on-execute-p t)
-(setq py-switch-buffers-on-execute-p t)
+;; switch to the interpreter after executing code
+;; (setq py-shell-switch-buffers-on-execute-p t)
+;; (setq py-switch-buffers-on-execute-p t)
 
-; don't split windows
+;; don't split windows
 (setq py-split-windows-on-execute-p nil)
 
-; try to automagically figure out indentation
+;; try to automagically figure out indentation
 (setq py-smart-indentation t)
