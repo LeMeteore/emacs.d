c9e948ef1e2eaf87607b98b547775de7c9d5793f
patrick nsukami <ndkpatt@gmail.com>
2015-12-01 19:57:40 +0000

    enabled default theme: material

--- init.el
+++ init.el
@@ -24,10 +24,34 @@
  ;; If there is more than one, they won't work right.
  '(ansi-color-faces-vector
    [default default default italic underline success warning error])
- '(custom-enabled-themes (quote (misterioso)))
+ '(custom-enabled-themes (quote (material)))
  '(custom-safe-themes
    (quote
-    ("870a63a25a2756074e53e5ee28f3f890332ddc21f9e87d583c5387285e882099" default))))
+    ("870a63a25a2756074e53e5ee28f3f890332ddc21f9e87d583c5387285e882099" default)))
+ '(fci-rule-color "#3a3a3a")
+ '(hl-sexp-background-color "#121212")
+ '(vc-annotate-background nil)
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#f36c60")
+     (40 . "#ff9800")
+     (60 . "#fff59d")
+     (80 . "#8bc34a")
+     (100 . "#81d4fa")
+     (120 . "#4dd0e1")
+     (140 . "#b39ddb")
+     (160 . "#f36c60")
+     (180 . "#ff9800")
+     (200 . "#fff59d")
+     (220 . "#8bc34a")
+     (240 . "#81d4fa")
+     (260 . "#4dd0e1")
+     (280 . "#b39ddb")
+     (300 . "#f36c60")
+     (320 . "#ff9800")
+     (340 . "#fff59d")
+     (360 . "#8bc34a"))))
+ '(vc-annotate-very-old-color nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
