855fd50c4e6c460443e2b5e27b00a9202c610545
patrick nsukami <ndkpatt@gmail.com>
2016-02-29 21:56:19 +0000

    add history configuration

--- lisp/init_settings.el
+++ lisp/init_settings.el
@@ -325,3 +325,14 @@ display-time-mail-file nil)
 
 ;; copy from one windonw to the next
 (setq dired-dwim-target t)
+
+;; history
+(setq savehist-file "~/.emacs.d/savehist")
+(savehist-mode 1)
+(setq history-length t)
+(setq history-delete-duplicates t)
+(setq savehist-save-minibuffer-history 1)
+(setq savehist-additional-variables
+      '(kill-ring
+        search-ring
+        regexp-search-ring))
